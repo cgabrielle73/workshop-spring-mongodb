b8a0f0f6a763146750113046cf5c389ae66b77bf
Clara <clara.gabriele@youxgroup.com.br>
2021-11-22 10:06:59 -0300

    Inserção de usuário com POST e Deleção de usuário com DELETE

--- src/main/java/com/workshop/workshop/services/UserServices.java
+++ src/main/java/com/workshop/workshop/services/UserServices.java
@@ -1,6 +1,7 @@
 package com.workshop.workshop.services;
 
 import com.workshop.workshop.domain.User;
+import com.workshop.workshop.dto.UserDTO;
 import com.workshop.workshop.repository.UserRepository;
 import com.workshop.workshop.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,18 @@ public class UserServices {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
